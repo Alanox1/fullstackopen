4256df5d81dd766c50520175c6bbeec027a673f1
Alanox <alannm54@gmail.com>
2023-05-09 11:59:12 -0300

    actualizando el unicafe

--- part-01/unicafe/src/main.jsx
+++ part-01/unicafe/src/main.jsx
@@ -1,42 +1,57 @@
 import { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Statistics  = ({good,neutral,bad}) => {
+const Statistics  = ({good,neutral,bad,counter}) => {
 
-  if(!good && !neutral && !bad) return <p>No feedback given</p>
-  return (
-    <>
-    <p>Statistics</p>
-   
-         
-      <StatisticLine text="good" score={good} />
-      <StatisticLine text="neutral" score={neutral} />
-      <StatisticLine text="bad" score={bad} />
-      <StatisticLine text="all" score={good + neutral + bad}/>     
-      {/* <StatisticLine text="average" score={(2) / 3} /> */}
-    
+  if(!counter) {
+    return <>
+          <div>Statistics</div>
+          <p>No feedback given</p>
     </>
-    
-
-  )
+  }
+  else{
+    return (
+      <>
+      <p>Statistics</p>
+        <StatisticLine text="good" score={good} />
+        <StatisticLine text="neutral" score={neutral} />
+        <StatisticLine text="bad" score={bad} />
+        <StatisticLine text="all" score={good + neutral + bad}/>     
+        <StatisticLine text="average" score={(good + neutral + bad) / 3} />
+        <StatisticLine text="positives" score={"%" + (good / counter) * 100} />
+      </>  
+    )
+  }
 }
+
+
 const StatisticLine = ({text,score}) => <div>{text} {score}</div>
-
-
 
 const Button = ({text,handleClick}) => <button onClick={handleClick}>{text}</button>
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(6)
-  const [neutral, setNeutral] = useState(2)
-  const [bad, setBad] = useState(1)
- 
-  const handleGood = () => setGood(good + 1)
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [counter, setCounter] = useState(0);
+  
+  const handleGood = () => {
+    setGood(good + 1)
+    setCounter(counter + 1)
+  }
 
-  const handleNeutral = () => setNeutral(neutral + 1)
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)   
+    setCounter(counter + 1) 
+  } 
 
-  const handleBad = () => setBad(bad + 1)
+  const handleBad = () => {
+    setCounter(counter + 1)
+    setBad(bad + 1)
+  } 
+
+
   return (
     <>
     <div>
@@ -48,7 +63,7 @@ const App = () => {
       <Button text="bad" handleClick={handleBad} />
    
      <div>
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <Statistics good={good} neutral={neutral} bad={bad} counter={counter}/>
     </div>
     </>
    
